188cd5751477ac74a22b7d52d34ca9daa1538ebb
knking <knr020498@gmail.com>
2023-12-19 16:51:37 +0530

    day 1 liveclass assignment

---
+++ live-classes/day-1/simple-primitives.js
@@ -0,0 +1,24 @@
+
+
+// 1. Write the program to greet a person given their first and last name 
+
+let fname="krishna"
+let lastname="Roy"
+console.log("Namaste "+fname+" "+lastname)
+
+// 2. Write a program that greets a person based on their gender. (If else) 
+
+let fName="krishna"
+let gName="richa"
+let gender = "female"
+if(gender==="male"){
+    console.log("Namate  "+fName)
+}else{
+    console.log("Namaste "+gName)
+}
+
+// 3. Write a program that counts from 0 - 1000 and prints (for loop
+
+for(let i=0;i<=100;i++){
+    console.log(i)
+}
