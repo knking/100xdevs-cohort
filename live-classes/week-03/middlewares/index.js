f491209d04fa45e671b2df788e43db56cefbbed1
knking <knr020498@gmail.com>
2023-12-21 16:49:59 +0530

    input validation using ZOD

--- live-classes/week-03/middlewares/index.js
+++ live-classes/week-03/middlewares/index.js
@@ -1,5 +1,7 @@
 
 const express = require("express")
+const zod = require("zod")
+const mySchema = zod.array(zod.number())
 const app =express()
 
 const port =3000
@@ -25,13 +27,35 @@ const port =3000
 
 //example 2
 
+// app.use(express.json())
+// app.post('/check',(req,res)=>{
+//     const kidneys=req.body.kidneys;
+//     const kidneyLength = kidneys.length;
+// // if(!kidneys){
+// //     res.json({
+// //         msg:"Please use valid value"
+// //     })
+// //     return
+// // }
+//     res.send("You have " +kidneyLength + " kidnesy")
+// })
+
+// app.use((err,req,res,next)=>{
+// res.json({
+//     msg:"Wrong input bro"
+// })
+// })
+
+// ZOD Example 
+
 app.use(express.json())
+app.post('/health-check',(req,res)=>{
+const kidneys=req.body.kidneys;
+const response = mySchema.safeParse(kidneys)
+res.send({
+    response
+})
 
-app.send('/check',(req,res)=>{
-    const kidneys=req.body.kidneys;
-    const kidneyLength = kidneys.length;
-
-    res.send("You have " +kidneyLength+ "kidnesy")
 })
 app.listen(port,()=>{
     console.log("Server is up......")
