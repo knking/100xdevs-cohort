3d7330583b177b8f79ab522c7fb926ae92bcd548
knking <knr020498@gmail.com>
2023-12-22 23:07:10 +0530

    connect backend to database

--- live-classes/week-03/auth/database.js
+++ live-classes/week-03/auth/database.js
@@ -1,9 +1,35 @@
 
+const express= require('express')
 const mongoose = require('mongoose');
+const app= express()
+
+app.use(express.json())
 
 mongoose.connect('mongodb+srv://admin:9KUahbZMaRotgkuZ@cluster0.z8zlmqz.mongodb.net/userdemo');
 
 const User = mongoose.model('User', { name: String , email:String, password : String});
 
 const user = new User({ name: 'Krishna', eamil:'abc@123',password:"knr" });
-user.save()
+
+app.post("/signup",async function(req,res){
+  const username= req.body.username
+  const password = req.body.password
+  const name = req.body.name
+
+  const existingUser = await User.findOne({email:username})
+  if(existingUser){
+    return res.status(400).send("User alrrady exist")  
+  }
+  const user = new User({
+    name:name,
+    email:username,
+    password:password
+  })
+  user.save()
+  res.json({
+    msg:"User created successfully"
+  })
+})
+
+
+
