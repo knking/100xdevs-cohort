6ecb098859ab42bb6cd0e69d126977e95766675c
knking <knr020498@gmail.com>
2023-12-22 11:23:36 +0530

    learing authentication, jwt

--- live-classes/week-03/middlewares/index.js
+++ live-classes/week-03/middlewares/index.js
@@ -1,10 +1,11 @@
 
 const express = require("express")
 const zod = require("zod")
-const mySchema = zod.array(zod.number())
+
 const app =express()
 
 const port =3000
+
 
 // app.get('/health-checkup',(req,res)=>{
 //     const kidneyId= req.query.kidneyId;
@@ -47,11 +48,25 @@ const port =3000
 // })
 
 // ZOD Example 
+const mySchema = zod.array(zod.number())
 
+const schema  =zod.object({
+    email:zod.string(),
+    password:zod.string(),
+    country:zod.literal("IN").or(zod.literal("US"))
+
+})
 app.use(express.json())
 app.post('/health-check',(req,res)=>{
 const kidneys=req.body.kidneys;
 const response = mySchema.safeParse(kidneys)
+// one way to throw erro
+// if(!response){
+//     res.status(404).json({
+// msg:"input is not valid"
+//     })
+// }
+//another way to throw errro
 res.send({
     response
 })
