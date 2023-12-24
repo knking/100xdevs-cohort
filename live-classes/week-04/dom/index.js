2a1446424fa9922a615e7a2f0ad242ea2a8e0bf2
knking <knr020498@gmail.com>
2023-12-24 23:13:33 +0530

    sum server fineshed

--- live-classes/week-04/dom/index.js
+++ live-classes/week-04/dom/index.js
@@ -1,9 +1,25 @@
 
-function sum() {
+// function sum() {
+//     const a = document.getElementById("firstNumber").value;
+//     const b = document.getElementById("secondNumber").value;
+//     const element = document.getElementById("final-sum")
+//     // element.innerHTML=parseInt(a) + parseInt(b)
+//     fetch("https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b)
+//     .then(function(response){
+//         response.text()
+//         .then(function(ans){
+//             element.innerHTML=ans
+//         })
+//     })
+
+// }
+
+async function sum2(){
     const a = document.getElementById("firstNumber").value;
     const b = document.getElementById("secondNumber").value;
     const element = document.getElementById("final-sum")
-    // element.innerHTML=parseInt(a) + parseInt(b)
-    fetch("https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b)
-
+    
+    const response = await  fetch("https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b)
+    const ans = await response.text()
+    element.innerHTML=ans
 }
