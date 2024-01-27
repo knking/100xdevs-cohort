fb0a388f23636018416497af83f46bc992701dec
knking <knr020498@gmail.com>
2024-01-27 23:32:38 +0530

    week-03 Zod

--- live-classes/main.js
+++ live-classes/main.js
@@ -40,39 +40,25 @@ const transactions = [
 
 
 
-// const request = require('supertest');
-// const assert = require('assert');
-const express = require('express');
+const z = require("zod")
+// const mySchema = z.string();
 
-const app = express();
-let errorCount = 0;
+// const mySchema1 =z.array(z.number())
 
-app.get('/user', function(req, res) {
-  throw new Error("some error");
-  // 500 
-  res.status(200).json({ name: 'john' });
-});
+// const mySchema = z.array(z.string())
+const mySchema =  z.object(
+  {
+    email:z.string(),
+    name:z.string(),
+    country:z.literal("in").or(z.literal("us"))
+  }
+)
+const obj1 = {
+  email:"krishna",
+  name:"kn",
+  country:"in"
+  
+}
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
-});
 
-app.get('/errorCount', function(req, res) {
-  res.status(200).json({ errorCount });
-});
-
-
-// You have been given an express server which has a few endpoints.
-// Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
-
-// error handling middleware
-app.use(function(err, req, res, next) {
-  res.status(404).send({})
-  errorCount = errorCount + 1;
-})
-
-app.listen(3000,()=>{
-  console.log("Server is Running..")
-})
+console.log(mySchema.safeParse(obj1))
